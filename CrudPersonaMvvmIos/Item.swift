b5cdc371043f1f1ce32510d4a8f5a97e5f0ba15f
Eddy Mendoza <eddymy1304@gmail.com>
2025-03-19 23:46:54 -0500

    Initial Commit

---
+++ CrudPersonaMvvmIos/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CrudPersonaMvvmIos
+//
+//  Created by Eddy David Mendoza on 19/03/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
