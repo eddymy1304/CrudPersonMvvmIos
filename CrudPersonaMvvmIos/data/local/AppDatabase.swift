f132568eac726251a3521e34637d6b5ac1878673
Eddy Mendoza <eddymy1304@gmail.com>
2025-05-11 02:39:01 -0500

    progress create db swiftdata

---
+++ CrudPersonaMvvmIos/data/local/AppDatabase.swift
@@ -0,0 +1,47 @@
+//
+//  AppDatabase.swift
+//  CrudPersonaMvvmIos
+//
+//  Created by Eddy David Mendoza on 11/05/25.
+//
+import SwiftData
+
+class AppDatabase {
+    
+    let modelContainer : ModelContainer
+    
+    @MainActor
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    init(
+        for modelTypes: [any PersistentModel.Type],
+        isStoredInMemoryOnly: Bool = false,
+        configurationName: String = "CrudPersonaMvvmIosBd",
+        allowsSave: Bool = true,
+        enablesCloudKit: Bool = false,
+        groupContainer: ModelConfiguration.GroupContainer = .none,
+        cloudKitDb: ModelConfiguration.CloudKitDatabase = .none
+    ) {
+        
+        let schema = Schema(modelTypes)
+        
+        var configurations = ModelConfiguration(
+            configurationName,
+            schema: schema,
+            isStoredInMemoryOnly: isStoredInMemoryOnly,
+            allowsSave: allowsSave,
+            groupContainer: groupContainer,
+            cloudKitDatabase: cloudKitDb
+        )
+        
+        do {
+            self.modelContainer = try ModelContainer(for: schema, configurations: configurations)
+        } catch {
+            fatalError("Unable to initialize the ModelContainer: \(error)")
+        }
+        
+        
+    }
+}
